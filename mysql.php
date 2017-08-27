18a94781789897a826b67eec23ab13060425fce7
stealkillradio <stealkillradio@gmail.com>
2017-08-27 16:34:16 +0300

    Change database connection

--- mysql.php
+++ mysql.php
@@ -1,11 +1,11 @@
 <?php
 
-$link = @mysqli_connect('mysql', 'test', 'test');
+$link = @mysqli_connect('mysql', 'root', 'test');
 if(!$link) {
     die('failed to connect to the server: ' . mysqli_connect_error());
 }
 
-if(!@mysqli_select_db($link, 'testdb')) {
+if(!@mysqli_select_db($link, 'test_database')) {
     die('failed to connect to the database: ' . mysqli_error($link));
 }else{
     die('MySQL version is: ' . mysqli_get_server_info($link));
